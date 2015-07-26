edd50f86aefa64953a29d5589454b3771d3af01a
Vitaly Voskobovich <vitali.voskobovich@moonion.com>
2015-07-26 15:37:54 +0300

    First commit

---
+++ CopyValueBehavior.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace voskobovich\behaviors;
+
+use yii\base\Behavior;
+use yii\base\Exception;
+use yii\base\InvalidConfigException;
+use yii\db\ActiveRecord;
+
+
+/**
+ * Class CopyValueBehavior
+ * @package voskobovich\behaviors
+ */
+class CopyValueBehavior extends Behavior
+{
+    /**
+     * Attributes list
+     * Format:
+     *  [
+     *      'meta_title' => 'name'
+     *          or
+     *      'meta_title' => [
+     *          'attribute' => 'name',       -- required
+     *          'clearTags' => true \ false, -- NOT required
+     *          'maxLength' => integer       -- NOT required
+     *      ]
+     *  ]
+     * @var
+     */
+    public $attributes;
+
+    /**
+     * Clear from html tags?
+     * @var bool
+     */
+    public $clearTags = false;
+
+    /**
+     * Cut to a specified number of characters?
+     * @var bool|integer
+     */
+    public $maxLength = false;
+
+    /**
+     * Init
+     */
+    public function init()
+    {
+        if ($this->attributes == null) {
+            throw new InvalidConfigException('Property "attributes" must be set');
+        }
+
+        parent::init();
+    }
+
+    /**
+     * Events
+     * @return array
+     */
+    public function events()
+    {
+        return [
+            ActiveRecord::EVENT_BEFORE_INSERT => 'copyAttributes',
+            ActiveRecord::EVENT_BEFORE_UPDATE => 'copyAttributes',
+        ];
+    }
+
+    /**
+     * Processing
+     * @throws Exception
+     */
+    function copyAttributes()
+    {
+        $owner = $this->owner;
+
+        foreach ($this->attributes as $destination => $source) {
+            if (is_array($source)) {
+                if (empty($source['attribute'])) {
+                    throw new Exception('Where to get the data? Set key "attribute"!');
+                }
+
+                $sourceAttribute = $source['attribute'];
+                $clearTags = (bool)isset($source['clearTags']) ? $source['clearTags'] : $this->clearTags;
+                $maxLength = isset($source['maxLength']) ? $source['maxLength'] : $this->maxLength;
+
+                if (empty($owner->{$destination}) && !empty($owner->{$sourceAttribute})) {
+                    $value = $owner->{$sourceAttribute};
+
+                    $value = $clearTags ? strip_tags($value) : $value;
+                    $value = is_integer($maxLength) ? mb_substr($value, 0, $maxLength) : $value;
+
+                    $owner->{$destination} = $value;
+                }
+            } else {
+                if (empty($owner->{$destination}) && !empty($owner->{$source})) {
+
+                    $value = $owner->{$source};
+
+                    $value = $this->clearTags ? strip_tags($value) : $value;
+                    $value = is_integer($this->maxLength) ? mb_substr($value, 0, $this->maxLength) : $value;
+
+                    $owner->{$destination} = $value;
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+Yii2 Copy Value Behavior
+================================
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+composer require --prefer-dist voskobovich/yii2-copy-value-behavior "*"
+```
+
+or add
+
+```
+"voskobovich/yii2-copy-value-behavior": "*"
+```
+
+to the require section of your `composer.json` file.
